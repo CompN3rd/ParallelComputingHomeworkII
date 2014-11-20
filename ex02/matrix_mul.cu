e38194467d39401c862d2a35fc6b71299f7c0a72
Torsten Thoben <t.thoben@freenet.de>
2014-11-20 10:46:21 +0100

    Upload Exercise 2

---
+++ ex02/matrix_mul.cu
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+#include "cuda_utils.h"
+
+
+// CUDA kernel
+__global__ void matrix_mul(float* matrixA_d, float* matrixb_d int dimX, int dimY)
+{
+
+
+}
+ 
+
+int main( int argc, char* argv[] )
+{
+	int dimensionX = 1000;
+	int dimensionY = 1000;
+
+	float *matrixA_h, *matrixB_h;
+	
+	return 0;
+}
