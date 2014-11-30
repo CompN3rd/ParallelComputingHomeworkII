fc2bc81048a49515f1c7bf628eac04d03b38d2ed
Marc <marc.kassubeck@googlemail.com>
2014-11-30 13:22:38 +0100

    Added VisualStudio Project files (not compile tested)

--- ex03/laplace.cu
+++ ex03/laplace.cu
@@ -1,14 +1,21 @@
+#ifndef _WIN32
 #include "cuda_utils.h"
-#include "writeVTK.h"
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "writeVTK.h"
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
 #define NON_BOUND_ELEMENTS 2048
 #define GRID_SIZE 16
+#define NUM_ITERATIONS 20
 
 __device__ __host__ inline int getLinearIndex(int rowIndex, int colIndex, int arrayWidth)
 {
-	return rowIndex * gridWidth + colIndex;
+	return rowIndex * arrayWidth + colIndex;
 }
 
 __global__ void laplaceGlobalStep(float* currentTime, float* nextTime, int gridWidth, float alpha, float deltaT, float h)
@@ -18,29 +25,57 @@ __global__ void laplaceGlobalStep(float* currentTime, float* nextTime, int gridW
 	int colIndex = blockIdx.x * blockDim.x + threadIdx.x + 1;
 
 	// gridWidth includes boundary elements!
-	float currentPos = currrentTime[getLinearIndex(rowIndex, colIndex, gridWidth)];
-	float rightPos = currrentTime[getLinearIndex(rowIndex, colIndex + 1, gridWidth)];
-	float leftPos = currrentTime[getLinearIndex(rowIndex, colIndex - 1, gridWidth)];
-	float topPos = currrentTime[getLinearIndex(rowIndex - 1, colIndex, gridWidth)];
-	float bottomPos = currrentTime[getLinearIndex(rowIndex + 1, colIndex, gridWidth)];
+	float currentPos = currentTime[getLinearIndex(rowIndex, colIndex, gridWidth)];
+	float rightPos = currentTime[getLinearIndex(rowIndex, colIndex + 1, gridWidth)];
+	float leftPos = currentTime[getLinearIndex(rowIndex, colIndex - 1, gridWidth)];
+	float topPos = currentTime[getLinearIndex(rowIndex - 1, colIndex, gridWidth)];
+	float bottomPos = currentTime[getLinearIndex(rowIndex + 1, colIndex, gridWidth)];
 
 	// explicit finite differences formula
 	nextTime[getLinearIndex(rowIndex, colIndex, gridWidth)] = currentPos + alpha * deltaT / (h * h) * (topPos + bottomPos + rightPos + leftPos - 4 * currentPos);
 } 
 
+void initializeArrays(float* currentTime, float* nextTime, int arrayWidth)
+{
+	//initialize host memory:
+	for (int i = 0; i < arrayWidth; i++)
+	{
+		for (int j = 0; j < arrayWidth; j++)
+		{
+			if (i >= 3 * arrayWidth / 8 && i < 5 * arrayWidth / 8 && j >= arrayWidth / 3 && j < 2 * arrayWidth / 3)
+			{
+				currentTime[i * arrayWidth + j] = 100;
+				nextTime[i * arrayWidth + j] = 100;
+			}
+			else
+			{
+				currentTime[i * arrayWidth + j] = 0;
+				nextTime[i * arrayWidth + j] = 0;
+			}
+		}
+	}
+}
+
 int main()
 {
-	//global memory part
 	int arraySize = NON_BOUND_ELEMENTS + 2;
 
 	float* h_currentTime = (float*) malloc(arraySize * arraySize * sizeof(float));
 	float* h_nextTime = (float*) malloc(arraySize * arraySize * sizeof(float));
+
+	//intialize arrays:
+	initializeArrays(h_currentTime, h_nextTime, arraySize);
+
+	//initialize device memory
 	float* d_currentTime = NULL;
 	float* d_nextTime = NULL;
 
 	cudaMalloc((void**) &d_currentTime, arraySize * arraySize * sizeof(float));
+	cudaMalloc((void**) &d_nextTime, arraySize * arraySize * sizeof(float));
 
-		
+	//copy
+	cudaMemcpy(d_currentTime, h_currentTime, arraySize * arraySize * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_nextTime, h_nextTime, arraySize * arraySize * sizeof(float), cudaMemcpyHostToDevice);
 
 	return 0;
 }
